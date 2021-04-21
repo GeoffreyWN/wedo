95f52b03c0914840c79dc6185fde7273639739d6
weshsnr <ndungugeoffrey11@gmail.com>
2021-04-21 12:48:44 +0300

    updated project CRUD logic on the server

--- server/models/Project.js
+++ server/models/Project.js
@@ -2,15 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const projectSchema = new Schema({
-    title: {type: String, required: true},
-    startDate: {type: Date, required: true},
-    endDate: {type: Date, required: true},
-    description: {type: String, required: true},
-    priority: {type: String, required: true},
-    status: {type: String, required: true},
-    createdOn: {type: Date, default: Date.now },
-    updatedOn: {type: Date, default: Date.now },
-    // user: { type: Schema.Types.ObjectId, ref: 'users' }
+    title: { type: String, required: true },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+    description: { type: String, required: true },
+    priority: { type: String, required: true },
+    status: { type: String, required: true },
+    createdOn: { type: Date, default: Date.now },
+    updatedOn: { type: Date, default: Date.now },
+    deleted: { type: Boolean, required: true, default: false }
+    // user: { type: Schema.Types.ObjectId, ref: 'users' } TODO: link project to user
 })
 
 module.exports = mongoose.model('Project', projectSchema)
--- server/routes/projects.js
+++ server/routes/projects.js
@@ -6,7 +6,7 @@ const { body, validationResult } = require("express-validator");
 
 // @router POST /api/v1/project
 // @description /create project
-// @access Private
+// @access Private  TODO:
 
 router.post("/", [
     body('title', 'Project title is required').not().isEmpty().trim().escape().custom(value => {
@@ -38,7 +38,7 @@ router.post("/", [
 
             const project = await newProject.save()
             const response = {
-                "success": "new project added",
+                "msg": " success new project added",
                 "addedProject": project
             }
             res.json(response)
@@ -53,13 +53,18 @@ router.post("/", [
 
 // @router GET /api/v1/projects
 // @description /fetch all projects
-// @access Private
+// @access Private  TODO:
 router.get('/', async (req, res) => {
     try {
-        const projects = await Project.find().sort({ createdOn: -1 })
+        // await Project.updateMany({}, {$set: {deleted: false}}) added a deleted field to existing documents to reflect updated schema
+        const projects = await Project.where('deleted').equals(false).sort({ createdOn: -1 })
+        // const projects = await Project.find({deleted: true}).sort({createdOn: -1})
+        if (!projects) {
+            res.status(404).send('No projects found')
+        }
 
         const response = {
-            "success": "all projects fetched",
+            "msg": "success: all projects fetched",
             "foundProjects": projects
         }
 
@@ -70,9 +75,10 @@ router.get('/', async (req, res) => {
     }
 })
 
-//@router PUT /api/v1/project/:id/projects
+//@router PUT /api/v1/projects/update/:project_id/
 //@description /update a project
-//@access Private
+//@access Private  TODO:
+//TODO: check whether another project title exists before saving
 
 router.put('/update/:project_id',
     [
@@ -97,13 +103,11 @@ router.put('/update/:project_id',
                 return res.status(400).json({ msg: "Project not found" })
             }
 
-            const updatedProject = await Project.findOneAndUpdate({ '_id': project.id }, { $set: { title, startDate, endDate, description, priority, status, updatedOn: Date.now() } }, { new: true })
-
-
+            const updatedProject = await Project.findByIdAndUpdate({ '_id': project.id }, { $set: { title, startDate, endDate, description, priority, status, updatedOn: Date.now() } }, { new: true })
 
             const response = {
-                "success": "project was updated",
-                "updatedProject": updatedProject
+                "msg": " success project was updated",
+                "updatedProject": updatedProject,
             }
             res.json(response)
 
@@ -115,5 +119,98 @@ router.put('/update/:project_id',
             res.status(500).send('Server error')
         }
     })
+
+
+//@router PUT /api/v1/projects/remove/:project_id/
+//@description /soft delete a project
+//@access Private TODO:
+
+router.put('/remove/:project_id',
+    async (req, res) => {
+        try {
+            const project_id = req.params.project_id
+            const project = await Project.findById(project_id)
+
+            if (!project) {
+                return res.status(404).json({ msg: "Project not found" })
+            }
+
+            const deletedProject = await Project.findOneAndUpdate({ '_id': project.id }, { $set: { 'deleted': true, updatedOn: Date.now() } }, { new: true })
+
+            const response = {
+                "msg": " success project was removed",
+                "removedProject": deletedProject
+            }
+            res.json(response)
+
+        } catch (error) {
+            console.error(error.message);
+            if (error.kind === 'ObjectId') {
+                return res.status(422).json({ msg: "invalid project id" })
+            }
+            res.status(500).send('Server error')
+        }
+    })
+
+//@router DELETE /api/v1/projects/delete/:project_id/
+//@description /permanently delete a project
+//@access Private TODO:
+
+router.delete('/delete/:project_id',
+    async (req, res) => {
+        try {
+            const project_id = req.params.project_id
+            const project = await Project.findById(project_id)
+
+            if (!project) {
+                return res.status(404).json({ msg: "Project not found" })
+            }
+            // const deleted = await project.deleteOne()
+            const deletedProject = await Project.findOneAndDelete({ _id: project_id })
+
+
+            const response = {
+                "msg": " success project was deleted",
+                "deletedProject": deletedProject
+            }
+
+            res.json(response)
+
+        } catch (error) {
+            console.error(error.message);
+            if (error.kind === 'ObjectId') {
+                return res.status(422).json({ msg: "invalid project id" })
+            }
+            res.status(500).send('Server error')
+        }
+    })
+
+// @route /api/v1/projects/:project_id
+// @description fetch a single project
+// @aceess Private TODO:
+
+router.get('/:project_id', async (req, res) => {
+    const project_id = req.params.project_id
+    try {
+        const foundProject = await Project.findOne({ _id: project_id })
+
+        if (!foundProject) {
+            return res.status(400).json({ msg: "Project not found" })
+        }
+        const response = {
+            "msg": "success project found",
+            "deletedProject": foundProject
+        }
+        res.json(response)
+
+    } catch (error) {
+        console.error(error.message);
+        if (error.kind === 'ObjectId') {
+            return res.status(422).json({ msg: "invalid project id" })
+        }
+        res.status(500).send('Server error')
+    }
+})
+
 
 module.exports = router;
